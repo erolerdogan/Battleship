e84af2eb2e53bca8af2752fe3e97e988c9f13d43
erol.erdogan_thinkpad <erol.erdogan@generalmobile.com>
2019-12-05 06:10:07 -0800

    When all ships have been shot, the game is finished. Some improvement and bug fixing have been done.

--- BattleShip.py
+++ BattleShip.py
@@ -7,45 +7,45 @@ class Ships:
 
         row_alphabet = list(string.ascii_uppercase[0:10])
         dictionary = {row_alphabet[i]: i for i in range(10)}
-
-        print(dictionary)
-        print("   " + str(row_alphabet))
-        print("----------------------------------------------------------------")
         self.creating_matrix()
 
         counter = 0
-
         while True:
-            for m in range(10):
-                for n in range(10):
-                    if matrix[m][n] == " ":
-                        counter += 1
-                        if counter == 100:
-                            break
+            finished = 1
 
-            row_input = input("Row:").upper()
-            column_input = int(input("Column:"))
-            y = dictionary[row_input]
+            for x1 in range(0,10):
+                for y1 in range(0,10):
+                    if matrix[x1][y1] in range(1,5):
+                        finished = 0
 
-            if matrix[column_input-1][y] == " ":
-                print("You missed! Try again..")
-            elif matrix[column_input - 1][y] != "X" and matrix[column_input - 1][y] != " ":
-                print("Congratulations. You hit {} points!".format(matrix[column_input-1][y]))
-                matrix[column_input - 1][y] = "X"
+
+            if finished == 0:
+                row_input = input("Row:").upper()
+                column_input = int(input("Column:"))
+                y = dictionary[row_input]
+
+                if matrix[column_input-1][y] == " ":
+                    print("You missed! Try again..")
+
+                elif matrix[column_input - 1][y] != "X" and matrix[column_input - 1][y] != " ":
+                    print("Congratulations. You hit {} points!".format(matrix[column_input-1][y]))
+                    matrix[column_input - 1][y] = "X"
+
+                else:
+                    print("You have already hit here before!")
+                counter += 1
             else:
-                print("You have already hit here before!")
-
-
+                print("You finally finished {}. times".format(counter))
+                break
 
     def creating_matrix(self):
 
-        ships = [[1], [1], [1], [1], [2,2], [2,2], [2,2], [3,3,3], [3,3,3], [4,4,4,4]]
+        #To create 10x10 matrix, this comprehension didn't work for me interestingly
+        #   row = [" " for i in range(10)]
+        #   matrix = [row for j in range(10)]
+        ships = [[1], [1], [1], [1], [2, 2], [2, 2], [2, 2], [3, 3, 3], [3, 3, 3], [4, 4, 4, 4]]
 
-    #To create 10x10 matrix, this comprehension didn't work for me interestingly
-    #   row = [" " for i in range(10)]
-    #   matrix = [row for j in range(10)]
-
-    #To create 10x10 matrix
+        #To create 10x10 matrix
         for i in range(10):
             row = []
             for j in range(10):
@@ -78,14 +78,12 @@ class Ships:
                         for e in range(len(num)):
                             matrix[x + e][y] = num[e]
                         ships.remove(num)
-
                 else:
                     pass
 
-
+        #to visualize the table of matrix
         for m in range(len(matrix)):
-            print(str(m+1)+ "  " + str(matrix[m]))
-
+            print(str(m+1) + "  " + str(matrix[m]))
 
     def hitting(self, row, column):
 
